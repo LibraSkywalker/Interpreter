4285112054617ebc345f2c1031a9777c1649a792
LibraSkywalker <caiwanxin2009@hotmail.com>
2016-12-19 16:53:25 +0800

    Beyond Debug

--- skeleton/src/simpl/parser/ast/Seq.java
+++ skeleton/src/simpl/parser/ast/Seq.java
@@ -3,6 +3,7 @@ package simpl.parser.ast;
 import simpl.interpreter.RuntimeError;
 import simpl.interpreter.State;
 import simpl.interpreter.Value;
+import simpl.typing.Substitution;
 import simpl.typing.TypeEnv;
 import simpl.typing.TypeError;
 import simpl.typing.TypeResult;
@@ -19,13 +20,15 @@ public class Seq extends BinaryExpr {
 
     @Override
     public TypeResult typecheck(TypeEnv E) throws TypeError {
-        // TODO
-        return null;
+        TypeResult leftResult = l.typecheck(E);
+        TypeResult rightResult = r.typecheck(leftResult.s.compose(E));
+        Substitution compoundSubstitution = leftResult.s.compose(rightResult.s);
+        return TypeResult.of(compoundSubstitution, compoundSubstitution.apply(rightResult.t));
     }
 
     @Override
     public Value eval(State s) throws RuntimeError {
-        // TODO
-        return null;
+        l.eval(s);
+        return r.eval(s);
     }
 }
